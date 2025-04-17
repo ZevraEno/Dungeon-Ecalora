47a97e1906d3b0f9fdd6f6bf3891af44dec60ddc
zevra2 <greywarden.dp@gmail.com>
2025-04-17 16:55:32 +0200

    oui

--- src/main/java/be/dungeonEcalora/rolePlayGameManagement/api/models/security/forms/RegisterForm.java
+++ src/main/java/be/dungeonEcalora/rolePlayGameManagement/api/models/security/forms/RegisterForm.java
@@ -5,7 +5,6 @@ import be.dungeonEcalora.rolePlayGameManagement.dl.enums.Gender;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
